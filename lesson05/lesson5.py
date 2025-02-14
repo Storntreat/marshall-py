c39d8a4d418b214686c836686902fff8a04544b1
Kevin Shen <kevinshen@SEA-FKZS203.EDU.YRDSB.NET>
2025-02-14 14:46:14 -0500

    solved lesson 5

--- lesson05/lesson5.py
+++ lesson05/lesson5.py
@@ -1,1 +1,16 @@
 # Lesson 5
+
+money = float(input())
+orderlist = input()
+
+bigCookies = orderlist.count("b")
+cookies = orderlist.count("c")
+
+cost = cookies*0.5 + bigCookies*0.75
+revenue = cookies*1.25 + bigCookies*2
+
+profit = revenue-cost
+
+print(bigCookies + cookies)
+print(profit)
+print(profit + money)
