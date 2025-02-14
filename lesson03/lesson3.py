45beb9cf61b33b45ab9f8f449e2088dfb4d1afda
Kevin Shen <kevinshen@SEA-FKZS203.EDU.YRDSB.NET>
2025-02-14 14:01:57 -0500

    solved lesson 3

--- lesson03/lesson3.py
+++ lesson03/lesson3.py
@@ -1,1 +1,15 @@
 # Lesson 3
+area = input()
+area = int(area)
+
+# sideLength = 1     # original solution
+# while sideLength**2 <= area:
+#     sideLength += 1
+# else:
+#     sideLength -= 1
+#     print("The largest square has side length is", sideLength, ".")
+
+sideLength = area ** 0.5
+sideLength = int(sideLength // 1)
+
+print(f"The largest square has side length {sideLength}.")
