58768d106a57efcfe81da9dc713dbed2d230352a
Kevin Shen <kevinshen@SEA-FKZS203.EDU.YRDSB.NET>
2025-02-14 14:34:42 -0500

    solved lesson 4

--- lesson04/lesson4.py
+++ lesson04/lesson4.py
@@ -1,1 +1,16 @@
 # Lesson 4
+
+import math
+
+frequency = len(input())
+frequency += len(input())
+frequency += len(input())
+print(frequency)
+
+dozens = math.ceil(frequency / 12)
+print(dozens*12 - frequency)
+print(dozens*14.95)
+
+#git add lessonN.py
+#git commit -m "solved lesson N"
+#git push
