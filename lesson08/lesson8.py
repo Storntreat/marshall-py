d09fd010e6daf1beb98a5408f160020f9e1c62da
Kevin Shen <kevinshen@SEA-FCC29W3.EDU.YRDSB.NET>
2025-02-21 13:51:39 -0500

    Solved lesson8 without range function?

--- lesson08/lesson8.py
+++ lesson08/lesson8.py
@@ -7,4 +7,11 @@ while counter < 6:
     if 'W' in game:
         wins += 1
     counter += 1
-if game == range(1,3)
+if wins == 1 or wins == 2:
+    print("Group 3")
+elif wins == 3 or wins == 4:
+    print("Group 2")   
+elif wins == 5 or wins == 6:
+    print("Group 1")   
+else:
+    print("Disqualified")
