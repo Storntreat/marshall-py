08553bdbab0ed7837e24878de9e9848826897e74
Kevin Shen <kevinshen@SEA-FCC29W3.EDU.YRDSB.NET>
2025-02-21 14:24:06 -0500

    solved lesson 10

--- lesson10/lesson10.py
+++ lesson10/lesson10.py
@@ -1,1 +1,15 @@
 # Lesson 10
+number = input()
+
+telemarketer = True
+
+if number[0] != '8' and number[0] != '9':
+    telemarketer = False
+if number[1] != number[2]:
+    telemarketer = False
+if number[3] != '8' and number[3] != '9':
+    telemarketer = False
+if telemarketer == False:
+    print("Not Telemarketer")
+if telemarketer == True:
+    print("Telemarketer")
