cfcd8566126b6ecb0e4ca97de2f2cb348cf691ab
Kevin Shen <kevinshen@SEA-FCC29W3.EDU.YRDSB.NET>
2025-02-21 14:34:18 -0500

    solved lesosn 11

--- lesson11/lesson11.py
+++ lesson11/lesson11.py
@@ -1,1 +1,16 @@
 # Lesson 11
+coord = input()
+coord = coord.split(' ')
+coord = list(map(int, coord))
+x,y = coord
+
+if x < 0:
+    if y < 0:
+        print(3)
+    else:
+        print(2)
+else:
+    if y < 0:
+        print(4)
+    else:
+        print(1)
