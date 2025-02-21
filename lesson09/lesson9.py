bce2c7852c7158a30a1e7706d4cc5baf8cc8efae
Kevin Shen <kevinshen@SEA-FCC29W3.EDU.YRDSB.NET>
2025-02-21 14:12:56 -0500

    solved lesson 9

--- lesson09/lesson9.py
+++ lesson09/lesson9.py
@@ -1,1 +1,29 @@
 # Lesson 9
+import random
+validChoice = False
+while validChoice == False:
+    playerChoice = input("Enter a valid move: ")
+    if playerChoice in {'rock', 'scissors', 'paper'}:
+        validChoice = True
+
+AiChoice = random.choice(['rock', 'scissors', 'paper'])
+
+if playerChoice == AiChoice:
+    print("Tie")
+else:
+    if playerChoice == 'rock':
+        if AiChoice == 'scissors':
+            print("Win")
+        else:
+            print("Lose")
+    elif playerChoice == 'scissors':
+        if AiChoice == 'rock':
+            print("Lose")
+        else:
+            print("Win")
+    else: 
+        if AiChoice == 'rock':
+            print("Win")
+        else: 
+            print("Lose")
+
