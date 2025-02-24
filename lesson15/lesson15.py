6450e4a67220fd28d9f8dfe526ecaf71aeb7fd9b
Kevin Shen <kevinshen@SEA-H41T203.EDU.YRDSB.NET>
2025-02-24 13:06:39 -0500

    learned about lesson15

--- lesson15/lesson15.py
+++ lesson15/lesson15.py
@@ -1,1 +1,5 @@
 # Lesson 15
+
+#while loop is used when we don't know when to end, we initalize a condition variable and make it changeable in order for the loop to end
+#for loops looks at each value in an iterable object and assigns it to an iterating variable
+#recursion calls uppon itself
