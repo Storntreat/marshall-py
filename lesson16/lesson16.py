5a108bc95d0facb52b21c40e141a98e8abd77dd3
Kevin Shen <kevinshen@SEA-H41T203.EDU.YRDSB.NET>
2025-02-24 13:12:54 -0500

    solved lesson16

--- lesson16/lesson16.py
+++ lesson16/lesson16.py
@@ -1,1 +1,11 @@
 # Lesson 16
+numbers = range(1,51)
+for x in numbers:
+    if x % 3 == 0 and x % 5 == 0:
+        print("FizzBuzz")
+    elif x % 3 == 0:
+        print("Fizz")
+    elif x % 5 == 0:
+        print("Buzz")
+    else:
+        print(x)
