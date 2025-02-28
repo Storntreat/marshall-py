b6a9d183c73f603746bc30d6f3c0822a0d27cf02
Kevin Shen <kevinshen@MacBook-Air-894.local>
2025-02-28 14:52:06 -0500

    solved lesson 28

--- lesson28/lesson28.py
+++ lesson28/lesson28.py
@@ -1,1 +1,14 @@
 # Lesson 28
+userInput = input()
+i = 0
+j = len(userInput)-1
+condition = True
+while i < len(userInput)-1:
+    if userInput[i] != userInput[j]:
+        print("False")
+        condition = False
+        i = len(userInput)
+    i += 1
+    j -= 1
+if condition: 
+        print("True")
