237dfc3fcb3bc68e5c1759ae8a9ef1fd3a099b1f
Kevin Shen <kevinshen@MacBook-Air-894.local>
2025-02-28 14:44:16 -0500

    solved lesson 27

--- lesson27/lesson27.py
+++ lesson27/lesson27.py
@@ -1,1 +1,11 @@
 # Lesson 27
+
+def alphacleaner(text):
+    new = ""
+    for c in text:
+        if c.isalpha():
+            new += c
+    return new
+
+string = input()
+print(alphacleaner(string))
