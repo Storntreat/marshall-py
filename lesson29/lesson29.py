0dc6a87cabc969aebda0cbcc0bb0e2996dd7a713
Kevin Shen <kevinshen@MacBook-Air-894.local>
2025-03-04 14:52:17 -0500

    solved lesson 29

--- lesson29/lesson29.py
+++ lesson29/lesson29.py
@@ -1,1 +1,8 @@
 # Lesson 29
+a = input("Input string: ")
+counter = 0
+for i in range(len(a)):
+    if a[i].isalpha():
+        if a[i] not in "aeiou":
+            counter += 1
+print(f"Number of consonants is: {counter}.")
