8781ddff822495a7c0d39d84f5bc7b9c0c5b3b6b
Kevin Shen <kevinshen@MacBook-Air-894.local>
2025-03-04 20:37:43 -0500

    solved lesosn 31

--- lesson31/lesson31.py
+++ lesson31/lesson31.py
@@ -1,1 +1,13 @@
 # Lesson 31
+a_string = input()
+b_string = input()
+a_list = list(a_string)
+b_list = list(b_string)
+a_list.sort()
+b_list.sort()
+print(a_list)
+print(b_list)
+if a_list == b_list:
+    print("Anagrams")
+else:
+    print("Not Anagrams")
