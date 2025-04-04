60b2fc94fc49dd023914f84f31c0fefa99ef123c
Kevin Shen <kevinshen@SEA-5FHN4M3.EDU.YRDSB.NET>
2025-04-04 13:57:46 -0400

    w

--- classStuff/Recursion/problem3.py
+++ classStuff/Recursion/problem3.py
@@ -3,15 +3,10 @@ def palindrome(n):
         return True
     elif len(n) == 1:
         return True
-    elif len(n) == 2:
-        if n[0] == n[1]:
-            return True
-        else:
-            return False
     else:
         if n[0] == n[-1]:
             return palindrome(n[1:len(n)-1])
         else:
             return False
 
-print(palindrome("tacocat"))
+print(palindrome("saippuakivikaupwpias"))
