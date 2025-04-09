7c75e964447d87b1b686f22514639e5ee49973bf
Kevin Shen <kevinshenhs@gmail.com>
2025-04-09 14:58:45 -0400

    solved lesson 34

--- lesson34/lesson34.py
+++ lesson34/lesson34.py
@@ -1,1 +1,9 @@
 # Lesson 34
+
+a_string = input()
+string_list = a_string.split(",")
+for i in range(len(string_list)):
+    string_list[i] = int(string_list[i])
+#or for value in a_string.split(","):
+#new_list.append(value)
+print(string_list)
